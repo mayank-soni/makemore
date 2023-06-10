39e24d2d48bba573e9a2e291c3b0de6021841086
Mayank Soni <mayank.soni101@gmail.com>
2023-06-11 07:56:36 +0800

    Modularise data, bigram model.

---
+++ src/bigram/bigram.py
@@ -0,0 +1,169 @@
+import string
+
+import matplotlib.pyplot as plt
+import torch
+
+from src.data.names import Names
+
+
+class BigramModel:
+    """Bigram character-level language model for names dataset"""
+
+    chars = list(string.ascii_lowercase)
+    ends = "[ENDS]"
+
+    def __init__(self, names: Names, reg: int = 1):
+        """
+        Args:
+            names (Names): Data class for names dataset
+            reg (int, optional): Regularisation parameter. Defaults to 1.
+        """
+        self.names = names
+        self.chars = self.get_chars()
+        self.index2char, self.char2index = self.get_mapping()
+        self.reg = reg
+        self.bigram_array = torch.Tensor
+        self.bigram_probs = torch.Tensor
+
+    def get_chars(self) -> list[str]:
+        """Starts with lowercase English letters, adds any other characters in names"""
+        self.chars = BigramModel.chars
+        for name in self.names.data:
+            for char in name:
+                if char not in self.chars:
+                    self.chars.append(char)
+        # Add start/end of word character
+        if BigramModel.ends not in self.chars:
+            self.chars.append(BigramModel.ends)
+        return self.chars
+
+    def get_mapping(self) -> tuple[dict[int, str], dict[str, int]]:
+        """Creates mappings between characters and indices for later use"""
+        self.index2char = {i: char for i, char in enumerate(self.chars)}
+        self.char2index = {char: i for i, char in enumerate(self.chars)}
+        return self.index2char, self.char2index
+
+    def train(self):
+        """Trains bigram model on training data. See create_bigram_array for details.
+        Also creates a matrix of probabilities from the bigram_array (normalised by row)
+        Adds regularisation to avoid zero probabilities
+        """
+        self.bigram_array = self.create_bigram_array(self.names.train) + self.reg
+        self.bigram_probs = self.bigram_array / self.bigram_array.sum(
+            axis=1, keepdims=True
+        )
+
+    def create_bigram_array(self, names: list[str]) -> torch.Tensor:
+        """Creates bigram array from list of names.
+        In effect, counts number of times each character appears after another and stores counts in a matrix.
+        Rows are first character, columns are second character.
+        Last row/column are for start/end of word.
+        """
+        # Initialise bigram array to zeros. Add one row/column to account for start/end of word
+        bigram_array = torch.zeros(len(self.chars), len(self.chars), dtype=torch.int64)
+        for ch1, ch2 in BigramModel.create_bigram_list(names):
+            bigram_array[self.char2index[ch1]][self.char2index[ch2]] += 1
+        return bigram_array
+
+    @classmethod
+    def create_bigram_list(cls, names: list[str]) -> list[tuple[str, str]]:
+        """Creates list of bigrams from list of names.
+        Start/end of word is represented by None.
+        """
+        bigrams = []
+        for name in names:
+            name = [BigramModel.ends] + list(name) + [BigramModel.ends]
+            for ch1, ch2 in zip(name, name[1:]):
+                bigrams.append((ch1, ch2))
+        return bigrams
+
+    def visualise(self):
+        """Visualises bigram array as a heatmap"""
+        plt.figure(figsize=(16, 16))
+        plt.imshow(self.bigram_array, cmap="Blues")
+        for i in range(self.bigram_array.shape[0]):
+            for j in range(self.bigram_array.shape[1]):
+                first = self.index2char[i]
+                # Shorten start/end of word to 'S'/'E' for readability
+                if first == BigramModel.ends:
+                    first = "S"
+                second = self.index2char[j]
+                if second == BigramModel.ends:
+                    second = "E"
+                chars = first + second
+                # Need parsemath=False to avoid error with '$$'
+                plt.text(
+                    j, i, chars, ha="center", va="top", color="gray", parse_math=False
+                )
+                plt.text(
+                    j,
+                    i,
+                    self.bigram_array[i, j].item(),
+                    ha="center",
+                    va="bottom",
+                    color="red",
+                )
+        plt.xlabel("Second Character")
+        plt.ylabel("First Character")
+
+    def predict_next(self, index: int, generator: torch._C.Generator) -> int:
+        """Predicts next character given current character.
+        Args:
+            index (int): Index of current character
+            generator (torch._C.Generator): Random number generator
+        Returns:
+            int: Index of next character
+        """
+        if generator:
+            prediction = torch.multinomial(
+                self.bigram_probs[index], 1, generator=generator
+            ).item()
+        else:
+            # If no generator, use default random number generator
+            prediction = torch.multinomial(self.bigram_probs[index], 1).item()
+        return prediction
+
+    def predict(
+        self, generator: torch._C.Generator = None, max_length: int = None
+    ) -> str:
+        """Generates a name from the model.
+        Args:
+            generator (torch._C.Generator, optional): Random number generator. Defaults to None.
+            max_length (int, optional): Maximum length of name to be generated. Defaults to None.
+        """
+        # Get index representing start/end of word
+        ends_index = self.char2index[BigramModel.ends]
+        word_indices = [ends_index]
+        # Keep iteratively generating characters until end of word is predicted.
+        # Or until max_length is reached
+        while True:
+            next_index = self.predict_next(word_indices[-1], generator)
+            if next_index == ends_index:
+                break
+            word_indices.append(next_index)
+            if max_length and len(word_indices) > max_length:
+                break
+        # Convert indices to characters, join up, and return
+        return "".join([self.index2char[i] for i in word_indices[1:]])
+
+    def evaluate(self) -> dict[str, float]:
+        """Calculates loss on training and test data.
+        Also calculates loss for baseline model with even probabilities."""
+        # Get bigram array for test data
+        test_array = self.create_bigram_array(self.names.test)
+        # Get baseline probability array with even probabilities
+        even_probs = torch.full((len(self.chars), len(self.chars)), 1 / len(self.chars))
+        baseline_train_loss = self.loss(self.bigram_array, even_probs)
+        baseline_test_loss = self.loss(test_array, even_probs)
+        model_train_loss = self.loss(self.bigram_array, self.bigram_probs)
+        model_test_loss = self.loss(test_array, self.bigram_probs)
+        return {
+            "baseline_train_loss": baseline_train_loss,
+            "baseline_test_loss": baseline_test_loss,
+            "model_train_loss": model_train_loss,
+            "model_test_loss": model_test_loss,
+        }
+
+    def loss(self, bigram_array, probs) -> float:
+        # average negative log-likelihood
+        return -1 * torch.nansum(bigram_array * probs.log()) / (bigram_array.sum())
---
+++ src/nnet/nnet.py
@@ -0,0 +1,65 @@
+import torch
+
+from src.bigram.bigram import BigramModel
+from src.data.names import Names
+
+
+class BigramNnet(torch.nn.Module):
+    def __init__(self, names: Names, reg: float = 0.1):
+        super().__init__()
+        self.names = names
+        self.bigram = BigramModel(self.names)
+        self.x_train, self.y_train, self.x_test, self.y_test = (
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+        )
+        # n_neurons needs to be the same as the number of potential output characters
+        self.n_neurons = len(self.bigram.chars)
+        # One-hot encoding of characters (embedding size = number of characters)
+        self.n_embedding = len(self.bigram.chars)
+        # Single hidden layer which is also the output layer. No biases, just weights
+        self.layer = torch.randn(
+            (self.n_embedding, self.n_neurons),
+            generator=torch.Generator().manual_seed(36),
+            requires_grad=True,
+        )
+        self.reg = reg
+
+    def get_data(self):
+        self.x_train, self.y_train = self.create_bigram_tensors(self.names.train)
+        self.x_test, self.y_test = self.create_bigram_tensors(self.names.test)
+
+    def create_bigram_tensors(self, names: list[str]) -> tuple[torch.Tensor]:
+        x, y = [], []
+        bigrams = BigramModel.create_bigram_list(names)
+        for bigram in bigrams:
+            x.append(self.bigram.char2index[bigram[0]])
+            y.append(self.bigram.char2index[bigram[1]])
+        x = torch.tensor(x, dtype=torch.int64)
+        x_enc = torch.nn.functional.one_hot(
+            x, num_classes=len(self.bigram.chars)
+        ).float()
+        y = torch.tensor(y, dtype=torch.int64)
+        return x_enc, y
+
+    def forward(self, x):
+        """Forward pass of a single layer."""
+        # Neuron activations
+        # n_samples x n_neurons = (n_samples x n_embedding) @ (n_embedding x n_neurons)
+        output = x @ self.layer
+        # Softmax for probs
+        probs = torch.softmax(output, dim=1)
+        return probs
+
+    def loss(self, probs, y_true):
+        # CCE Loss
+        # Extract probabilities for correct characters (i.e. each character in y)
+        # for each row (each value in the arange), extract the yth value
+        prob_correct = probs[torch.arange(len(y_true)), y_true]
+        # log the probs and take the negative and mean. Add L2 regularisation
+        loss = (-1 * torch.log(prob_correct).sum() / len(y_true)) + (
+            self.reg * torch.sum(self.layer**2)
+        )
+        return loss
